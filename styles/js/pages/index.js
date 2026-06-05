b3fc7e5ad9fbbc6530be6bd76b1394d27023ba72
Fulmine <jake@pals.gay>
2026-06-05 15:28:08 +0200

    fix: sort array before splice

--- styles/js/pages/index.js
+++ styles/js/pages/index.js
@@ -13,9 +13,13 @@ document.addEventListener("DOMContentLoaded", async () => {
   ===================================================================== */
   let designs = await charadex.initialize.page(null, charadex.page.index.designs, (arr) => {
 
+    // Sort the array in desc order
+    arr.sort((a,b) => b.id - a.id);
+
     // Splice the silly little array
     let sliceAmount = charadex.page.index.designs.amount || 6;
     arr.splice(sliceAmount, arr.length);
+    arr.reverse();
 
   });
 
@@ -23,9 +27,13 @@ document.addEventListener("DOMContentLoaded", async () => {
   ===================================================================== */
   let gallery = await charadex.initialize.page(null, charadex.page.index.gallery, (arr) => {
 
+    // Sort the array in desc order
+    arr.sort((a,b) => b.timestamp - a.timestamp);
+
     // Splice the silly little array
     let sliceAmount = charadex.page.index.gallery.amount || 6;
     arr.splice(sliceAmount, arr.length);
+    arr.reverse();
 
   });
 
